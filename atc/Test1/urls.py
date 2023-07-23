118ccdc1a89f133825076624bc280d5fa80b04d4
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-07-23 23:36:54 +0545

    created the path to all pages

--- atc/Test1/urls.py
+++ atc/Test1/urls.py
@@ -5,6 +5,15 @@ from . import views
 
 urlpatterns = [
 
+    path('', views.index, name=""),
 
-    
+    path('register', views.register, name="register"),
+
+    path('dashboard', views.dashboard, name="dashboard"),
+
+    path('my-login', views.my_login, name="my-login"),
+
+    path('profile-management', views.profile_management, name="profile_management"),
+
+
 ]
