ef919daab571f6ddb2c813f0a4f388d722c98be5
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-07-25 16:22:33 +0545

    Added a form and do validation

--- atc/Test1/views.py
+++ atc/Test1/views.py
@@ -1,15 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
+from . forms import CreateUSerForm
 
 def index(request):
 
     return render(request, 'test1/index.html')
 
+
+
 def register(request):
 
-    return render(request, 'test1/register.html')
+    form = CreateUSerForm()
+
+    if request.method == 'POST':
+
+        form = CreateUSerForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("my-login")
+        
+
+    context = { 'form': form }
+
+
+    return render(request, 'test1/register.html', context=context)
+
+
+
+
+
+
 
 def dashboard(request):
 
