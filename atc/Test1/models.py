bc39e7bcbaf13080022e9c11d624f77ffa2e9c65
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-07-25 15:04:38 +0545

    Added Img to default function

--- atc/Test1/models.py
+++ atc/Test1/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 
-    profile_pic = models.ImageField(null=True, blank=True, default='')
+    profile_pic = models.ImageField(null=True, blank=True, default='Default.png')
 
     
     user = models.ForeignKey(User, max_length=10, on_delete=models.CASCADE, null=True)
