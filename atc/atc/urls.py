602d4671874f0180ad8d7e5c31992ae7bc20a432
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-07-25 15:03:58 +0545

    Added Media url n imported settings, static

--- atc/atc/urls.py
+++ atc/atc/urls.py
@@ -2,6 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+
+from django.conf.urls.static import static
+
 urlpatterns = [
 
     path('admin/', admin.site.urls),
@@ -9,3 +13,5 @@ urlpatterns = [
     path('', include('Test1.urls')),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
